c488b948e650f31fdd0b6aa3f4e94a2c11fa733c
Daniel Charles <danielcharles_500@hotmail.com>
2026-02-21 22:35:00 +0000

    check

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Use the list above
+    allow_origins=["https://sci-fi-movie-landing-page.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
